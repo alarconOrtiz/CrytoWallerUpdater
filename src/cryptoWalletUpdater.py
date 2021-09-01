4f7fe20ac1eea0fe1208a8e09156db81a5c82064
alarcon <miguel.alarcon.ortiz@gmail.com>
2021-09-01 21:24:51 +0200

    update main to read configuration from file

--- src/cryptoWalletUpdater.py
+++ src/cryptoWalletUpdater.py
@@ -1,6 +1,16 @@
 #!/Users/alarcon/opt/anaconda3/bin/python3
+from ConfigHandler import ConfigHandler
+
+
 def main():
-    print("Hello World!")
+    #Getting the config information from a config file
+    config = ConfigHandler();
+    config.read_file()
+    print("Parameters.....")
+    print("Path: {} ".format(config.file_path_config))
+    print("Coin: {} ".format(config.crypto_coins))
+    print("excell: {}".format(config.excell_path))
+
 
 if __name__ == "__main__":
     main()

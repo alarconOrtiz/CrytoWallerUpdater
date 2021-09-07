9022fc49b9eb56e84161673f8a9b4f1f271366cc
alarcon <miguel.alarcon.ortiz@gmail.com>
2021-09-07 17:29:12 +0200

    update repo

--- src/cryptoWalletUpdater.py
+++ src/cryptoWalletUpdater.py
@@ -1,15 +1,22 @@
 #!/Users/alarcon/opt/anaconda3/bin/python3
+from CoinMarketAPI import CoinMarketApi
 from ConfigHandler import ConfigHandler
 
 
 def main():
     #Getting the config information from a config file
-    config = ConfigHandler();
+    config = ConfigHandler()
     config.read_file()
     print("Parameters.....")
     print("Path: {} ".format(config.file_path_config))
     print("Coin: {} ".format(config.crypto_coins))
     print("excell: {}".format(config.excell_path))
+    print("..................")
+
+    coinMarket = CoinMarketApi()
+    coinMarket.readCryptoEuroValue(config.crypto_coins)
+
+
 
 
 if __name__ == "__main__":
